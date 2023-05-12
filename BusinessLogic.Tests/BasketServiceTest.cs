b4763d9f870978f68ee99c850735f6eec806f81f
Ekaterina Gorina <108774312+joke04@users.noreply.github.com>
2023-05-12 13:42:33 +0300

    new

---
+++ BusinessLogic.Tests/BasketServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using BusinessLogic.Services;
+using DataAccess.Wrapper;
+using Domain.Interfaces;
+using Domain.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tests
+{
+    public class BasketServiceTest
+    {
+        private readonly BasketService service;
+        private readonly Mock<IBasketRepository> basketRepositoryMoq;
+
+        public BasketServiceTest()
+        {
+            var repositoryWrapperMoq = new Mock<IRepositoryWrapper>();
+            basketRepositoryMoq = new Mock<IBasketRepository>();
+
+            repositoryWrapperMoq.Setup(meow => meow.Basket)
+                .Returns(basketRepositoryMoq.Object);
+
+            service = new BasketService(repositoryWrapperMoq.Object);
+        }
+        public static IEnumerable<object[]> GetIncorrectBasket()
+        {
+            return new List<object[]>
+            {
+                new object[] { new Basket() },
+                new object[] { new Basket() { BasketNumber = 101 } },
+                new object[] { new Basket() { BasketNumber = 101, QuantityOfGoods = 10 } }
+            };
+        }
+
+        [Fact]
+        public async Task CreateAsync_NullUser_ShouldThrowNullExep()
+        {
+            /* act */
+            var ex = await Assert.ThrowsAnyAsync<ArgumentNullException>(() => service.Create(null));
+
+            /* assert */
+            Assert.IsType<ArgumentNullException>(ex);
+            basketRepositoryMoq.Verify(meow => meow.Create(It.IsAny<Basket>()), Times.Never);
+        }
+        [Theory]
+        [MemberData(nameof(GetIncorrectBasket))]
+        public async Task CreateAsyncNewUser_ShouldNot(Basket manufacturer)
+        {
+            /* arrange */
+            var newCustomer = manufacturer;
+
+            /* act */
+            var ex = await Assert.ThrowsAnyAsync<ArgumentException>(() => service.Create(newCustomer));
+
+            /* assert */
+            basketRepositoryMoq.Verify(meow => meow.Create(It.IsAny<Basket>()), Times.Never);
+            Assert.IsType<ArgumentException>(ex);
+        }
+        [Fact]
+        public async Task CreateAsyncNewUser_Should()
+        {
+            /* arrange */
+            var newCustomer = new Basket() { BasketNumber = 102, QuantityOfGoods = 10, UserIdd = 1 };
+
+            /* act */
+            await service.Create(newCustomer);
+
+            /* assert */
+            basketRepositoryMoq.Verify(meow => meow.Create(It.IsAny<Basket>()), Times.Once);
+        }
+    }
+}
---
+++ BusinessLogic.Tests/ProductServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using BusinessLogic.Services;
+using DataAccess.Wrapper;
+using Domain.Interfaces;
+using Domain.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tests
+{
+    public class ProductServiceTest
+    {
+        private readonly ProductService service;
+        private readonly Mock<IProductRepository> productRepositoryMoq;
+
+        public ProductServiceTest()
+        {
+            var repositoryWrapperMoq = new Mock<IRepositoryWrapper>();
+            productRepositoryMoq = new Mock<IProductRepository>();
+
+            repositoryWrapperMoq.Setup(ex => ex.Product)
+                .Returns(productRepositoryMoq.Object);
+
+            service = new ProductService(repositoryWrapperMoq.Object);
+        }
+        public static IEnumerable<object[]> GetIncorrectProduct()
+        {
+            return new List<object[]>
+            {
+                new object[] { new Product() },
+                new object[] { new Product() { NumberProduct = 101 } },
+                new object[] { new Product() { NumberProduct = 101, ProductDescription = "Text" } }
+            };
+        }
+
+        [Fact]
+        public async Task CreateAsync_NullUser_ShouldThrowNullExep()
+        {
+            /* act */
+            var ex = await Assert.ThrowsAnyAsync<ArgumentNullException>(() => service.Create(null));
+
+            /* assert */
+            Assert.IsType<ArgumentNullException>(ex);
+            productRepositoryMoq.Verify(meow => meow.Create(It.IsAny<Product>()), Times.Never);
+        }
+        [Theory]
+        [MemberData(nameof(GetIncorrectProduct))]
+        public async Task CreateAsyncNewUser_ShouldNot(Product product)
+        {
+            /* arrange */
+            var newProduct = product;
+
+            /* act */
+            var ex = await Assert.ThrowsAnyAsync<ArgumentException>(() => service.Create(newProduct));
+
+            /* assert */
+            productRepositoryMoq.Verify(meow => meow.Create(It.IsAny<Product>()), Times.Never);
+            Assert.IsType<ArgumentException>(ex);
+        }
+        [Fact]
+        public async Task CreateAsyncNewUser_Should()
+        {
+            /* arrange */
+            var newProd = new Product() { ProductPrice = 5.5m, NumberProduct = 101, Namee = "Text", IdCategories = 1, ProductDescription = "descr of text", Article = 10 };
+
+            /* act */
+            await service.Create(newProd);
+
+            /* assert */
+            productRepositoryMoq.Verify(meow => meow.Create(It.IsAny<Product>()), Times.Once);
+        }
+    }
+}
